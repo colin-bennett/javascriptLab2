a9c73575b0bc7f7cc3d12addb4ccd34537854a8f
Colin Bennett <colintb@outlook.com>
2019-07-23 16:53:28 -0400

    Functional Lab 2 code

---
+++ scripts/scripts.js
@@ -0,0 +1,56 @@
+const randomDamage = () => {
+  return Math.floor(Math.random() * 10 + 1);
+};
+// console.log(randomDamage());
+
+const chooseOption = (opt1, opt2) => {
+  let randNum = Math.round(Math.random());
+  return randNum === 0 ? opt1 : opt2;
+};
+// console.log(chooseOption("Mitch", "Adam"));
+
+const attackPlayer = function(health) {
+  return health - randomDamage();
+};
+
+const logHealth = (player, health) => {
+  console.log(`${player} health: ${health}`);
+};
+
+const logDeath = (winner, loser) => {
+  console.log(`${winner} defeated ${loser}`);
+};
+
+const isDead = health => {
+  return health <= 0;
+};
+
+// Function Declaration
+
+function fight() {
+  let player1 = "Mitch";
+  let player2 = "Adam";
+  let player1Health = 100;
+  let player2Health = 100;
+
+  while (true) {
+    let attacker = chooseOption(player1, player2);
+    if (attacker === player1) {
+      player2Health = attackPlayer(player2Health);
+      logHealth(player2, player2Health);
+      if (isDead(player2Health) === true) {
+        logDeath(player1, player2);
+        break;
+      }
+    } else {
+      player1Health = attackPlayer(player1Health);
+      logHealth(player1, player1Health);
+      if (isDead(player1Health) === true) {
+        logDeath(player2, player1);
+        break;
+      }
+    }
+  }
+}
+
+fight();
